ea217cf1a955337c73a0e6dbcadc4f1046735c04
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-13 17:12:08 +0000

    Parse(): Accepts a JSON string as a parameter, and returns the corresponding JS object.

---
+++ Webscraping/parse_json.js
@@ -0,0 +1,6 @@
+const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+const parsedObject = JSON.parse(jsonString);
+
+console.log(parsedObject.name);
+console.log(parsedObject.age); 
+console.log(parsedObject.city); 
