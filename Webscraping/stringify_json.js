1ea192fbb283d724a04223c5428d2ff94d42e490
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-13 17:15:28 +0000

    Stringify(): Accepts an object as a parameter, and return the corresponding Json.

---
+++ Webscraping/stringify_json.js
@@ -0,0 +1,10 @@
+const myObject = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+
+// Convert the object to a JSON string
+const jsonString = JSON.stringify(myObject);
+
+console.log(jsonString);
