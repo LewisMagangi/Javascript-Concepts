ec66c080d7341f4c816588ba74aa7b6798c88c17
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-13 18:42:12 +0000

    Usage of backticks in js.

---
+++ Basics/backtick_salamu.js
@@ -0,0 +1,8 @@
+// Basic usage of template literals
+const name = 'Lewis';
+const age = 23;
+
+// Using template literals to create a string
+const salamu = `Hello, my name is ${name} and I am ${age} years old.`;
+
+console.log(salamu);
---
+++ Basics/literal_strings.js
@@ -0,0 +1,8 @@
+// Multiline string using template literals
+const multilineString = `
+  This is a multiline string.
+  It can span multiple lines without the need for explicit line breaks.
+  Very convenient!
+`;
+
+console.log(multilineString);
